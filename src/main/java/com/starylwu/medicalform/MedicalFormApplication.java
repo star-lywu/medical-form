5688a43a288dd44e74db8edf2e23cd0e673b48e9
wuyulong <l>
2018-10-28 18:20:00 +0800

    添加mybatis

--- src/main/java/com/starylwu/medicalform/MedicalFormApplication.java
+++ src/main/java/com/starylwu/medicalform/MedicalFormApplication.java
@@ -1,9 +1,11 @@
 package com.starylwu.medicalform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.starylwu.medicalform.dao"})
 public class MedicalFormApplication {
 
     public static void main(String[] args) {
